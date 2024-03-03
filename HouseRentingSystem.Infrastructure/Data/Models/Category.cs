9edd107c5e34ec294254afea3d7991f3ca890b73
Daniel Nikolov <126501@students.ue-varna.bg>
2024-03-03 18:03:52 +0200

    Database created

---
+++ HouseRentingSystem.Infrastructure/Data/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static HouseRentingSystem.Infrastructure.Constants.DataConstants;
+
+namespace HouseRentingSystem.Infrastructure.Data.Models
+{
+    [Comment("House category")]
+    public class Category
+    {
+        [Key]
+        [Comment("Category Identifier")]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; } = string.Empty;
+
+        public List<House> Houses { get; set; } = new List<House>();
+    }
+}
