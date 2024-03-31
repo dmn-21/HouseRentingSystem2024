c45df07fa5d09e1754afa79fe9047d83e59aad43
Daniel Nikolov <126501@students.ue-varna.bg>
2024-03-31 13:53:44 +0300

    WebApi and Details security added

---
+++ HouseRentingSystem/Controllers/StatisticApiController.cs
@@ -0,0 +1,25 @@
+﻿using HouseRentingSystem.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystem.Controllers
+{
+    [Route("api/statistic")]
+    [ApiController]
+    public class StatisticApiController : ControllerBase
+    {
+        private readonly IStatisticService statisticService;
+
+        public StatisticApiController(IStatisticService _statisticService)
+        {
+            statisticService = _statisticService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetStatistic()
+        {
+            var result = await statisticService.TotalAsync();
+
+            return Ok(result);
+        }
+    }
+}
